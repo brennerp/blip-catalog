73a788260f9b1fb2a2e289bdfdd44ba0cca5c314
Brenner Pacelli <brenner@take.net>
2019-03-14 12:42:20 -0300

    First commit. Finished tests for files import and made base grid.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper">
+      <div id="svg-browser">
+        <p>SVG files</p>
+        <div class="image-browser">
+          <div class="image-container" v-for="svg in svgs" :key="svg">
+            <img :alt="svg" :src="svg" />
+          </div>
+        </div>
+      </div>
+
+      <div id="png-browser">
+        <p>PNG files</p>
+        <div class="image-browser">
+          <div class="image-container" v-for="png in pngs" :key="png">
+            <img :alt="png" :src="png">
+          </div>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+function importAll(r) {
+  return r.keys().map(r);
+}
+
+const svgFiles = importAll(require.context('./assets/img/svg', true, /\.svg$/));
+const pngFiles = importAll(require.context('./assets/img/png', true, /\.png$/));
+const gifFiles = importAll(require.context('./assets/img/gif', true, /\.gif$/));
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      pngs: pngFiles,
+      svgs: svgFiles,
+      gifs: gifFiles,
+    };
   },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: #607B99;
+}
+
+.wrapper {
+  padding: 40px;
+}
+
+.image-browser {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.image-container {
+  width: 120px;
+  height: 120px;
+  padding: 10px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  border: 1px solid blue;
+}
+
+img {
+  max-width: 100%;
+  max-height: 100%;
 }
 </style>
