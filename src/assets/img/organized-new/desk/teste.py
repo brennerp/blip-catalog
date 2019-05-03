e80af9a9bd0aed2b8673edca93949a5c4b5656d8
Brenner Pacelli <brenner_pacelli@hotmail.com>
2019-05-03 11:49:22 -0300

    Finished rewriting svg library converter

--- src/assets/img/organized-new/desk/teste.py
+++ src/assets/img/organized-new/desk/teste.py
@@ -1,29 +1,31 @@
-import glob
 import sys
 import re
 import ntpath
+import os
 
-path='all/*.svg'
-svgFiles = glob.glob(path)
-
+path='all/'
 defs = open("defs.svg", "w")
 
 defs.write('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\n')
 defs.write('\t<defs>\n')
 
-for svgFile in svgFiles:
-  with open(svgFile) as svg:
-    fullSvg = svg.read()
+for root, dirs, files in os.walk(path):
+  for svgFile in files:
+    print(svgFile)
+    if svgFile.endswith(".svg"):
+      svgFilePath = os.path.join(root, svgFile)
+      with open(svgFilePath) as svg:
+        fullSvg = svg.read()
 
-    fileName = ntpath.basename(svgFile).split('.svg')[0]
+        fileName = ntpath.basename(svgFile).split('.svg')[0]
 
-    pathStart = fullSvg.find('>') + 1
-    pathEnd = fullSvg.find('</svg>')
+        pathStart = fullSvg.find('>') + 1
+        pathEnd = fullSvg.find('</svg>')
 
-    svgPath = fullSvg[pathStart : pathEnd]
+        svgPath = fullSvg[pathStart : pathEnd]
 
-    path = ''.join(['\t\t<symbol id="', fileName, '" viewBox="0 0 24 24">', svgPath, '</symbol>\n'])
-    defs.write(path)
+        path = ''.join(['\t\t<symbol id="', fileName, '" viewBox="0 0 24 24">', svgPath, '</symbol>\n'])
+        defs.write(path)
 
 defs.write('\t</defs>\n')
 defs.write('</svg>\n')
